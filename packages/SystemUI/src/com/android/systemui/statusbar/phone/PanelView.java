7570f12ba2f0e6cc77c2476cd56e46ebd835ed85
Anas Karbila <anaskarbila@aospa.co>
2016-12-10 18:40:23 +0100

    PanelView: add double tap to sleep on keyguard
    Change-Id: Ibd80dbaf749e891971f2f52a03a3d1ed2bf7222b
    Signed-off-by: RahifM <faizel326@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -24,8 +24,10 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.SystemClock;
+import android.os.PowerManager;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.InputDevice;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
@@ -135,6 +137,8 @@ public abstract class PanelView extends FrameLayout {
     private boolean mIgnoreXTouchSlop;
     private boolean mExpandLatencyTracking;
 
+    private GestureDetector mDoubleTapGestureListener;
+
     protected void onExpandingFinished() {
         mBar.onExpandingFinished();
     }
@@ -193,6 +197,18 @@ public abstract class PanelView extends FrameLayout {
 
     public PanelView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mDoubleTapGestureListener = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent event) {
+                final PowerManager pm = (PowerManager) context.getSystemService(
+                        Context.POWER_SERVICE);
+                pm.goToSleep(event.getEventTime());
+                return true;
+            }
+        });
+
         mFlingAnimationUtils = new FlingAnimationUtils(context, 0.6f /* maxLengthSeconds */,
                 0.6f /* speedUpFactor */);
         mFlingAnimationUtilsClosing = new FlingAnimationUtils(context, 0.5f /* maxLengthSeconds */,
@@ -390,6 +406,11 @@ public abstract class PanelView extends FrameLayout {
                 endMotionEvent(event, x, y, false /* forceCancel */);
                 break;
         }
+
+        if (mStatusBar.getBarState() == StatusBarState.KEYGUARD) {
+            mDoubleTapGestureListener.onTouchEvent(event);
+        }
+
         return !mGestureWaitForTouchSlop || mTracking;
     }
 
