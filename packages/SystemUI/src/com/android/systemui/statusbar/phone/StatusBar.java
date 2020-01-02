e1237b56a8ab99c64650c6e14ac9f60f4a55ee53
Alex <du.alexcruz@gmail.com>
2020-01-02 16:48:02 -0500

    Introduce lockscreen clock/date styles [1/2]
    Inspired by OnePlus and their recent options to switch lockscreen
    clock styles on the OP6, I took it upon myself to do the same with our ROM.
    
    This isn't as fancy as OnePlus' because is just using a list preference instead
    of adding in all the eye candy but it gets the job done!
    
    Below is a run down of what took place...
    
    - Manipulated the clockview format for both 12 and 24 hour to achieved the 'sammy' style
    - Made the sammy style optional and created a switch case for all 3.
    - Set padding, singeline and other layout changes programmatically to minimize the jank
    - Reworked/cleaned up some existing code (Huge thanks to @ezio84)
    - Created bold sammy by using html formatting, based off the sammy one
    - Created bold digital by using html formatting, based off the digital one
    
    - Left out the analog clocks this time around. Too much of a hassle.
    
    For right now there's five clock styles:
    
    Clock styles---
    - Hidden
    - Default
    - Digital (bold)
    - Sammy
    - Sammy (bold)
    
    Change-Id: I2cfa59f6347fe779a3cf6d9b5239a71cea098666

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2007,6 +2007,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.PULSE_ON_NEW_TRACKS),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.LOCKSCREEN_CLOCK_SELECTION),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -2026,6 +2029,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.PULSE_ON_NEW_TRACKS))) {
                 setPulseOnNewTracks();
+            } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.LOCKSCREEN_CLOCK_SELECTION))) {
+                updateKeyguardStatusSettings();
             }
         }
 
@@ -2034,7 +2039,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateQsPanelResources();
             setLockScreenMediaBlurLevel();
             setPulseOnNewTracks();
+            updateKeyguardStatusSettings();
         }
+    }
+
+    private void updateKeyguardStatusSettings() {
+        mNotificationPanelViewController.updateKeyguardStatusSettings();
     }
 
     private void setFpToDismissNotifications() {
