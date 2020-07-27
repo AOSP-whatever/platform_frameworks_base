AOSPA/android_frameworks_base-old

PanelView: add double tap to sleep on keyguard
Change-Id: Ibd80dbaf749e891971f2f52a03a3d1ed2bf7222b

packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -23,8 +23,10 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.PowerManager;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.InputDevice;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
@@ -129,6 +131,8 @@ public abstract class PanelView extends FrameLayout {
         }
     };
 
+    private GestureDetector mDoubleTapGestureListener;
+
     protected void onExpandingFinished() {
         mBar.onExpandingFinished();
     }
@@ -191,6 +195,18 @@ public abstract class PanelView extends FrameLayout {
 
     public PanelView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mDoubleTapGestureListener = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent event) {
+                final PowerManager pm = (PowerManager) context.getSystemService(
+                        Context.POWER_SERVICE);
+                pm.goToSleep(event.getEventTime());
+                return true;
+            }
+        });
+
         mFlingAnimationUtils = new FlingAnimationUtils(context, 0.6f);
         mBounceInterpolator = new BounceInterpolator();
         mFalsingManager = FalsingManager.getInstance(context);
@@ -354,6 +370,11 @@ public abstract class PanelView extends FrameLayout {
                 endMotionEvent(event, x, y, false /* forceCancel */);
                 break;
         }
+
+        if (mStatusBar.getBarState() == StatusBarState.KEYGUARD) {
+            mDoubleTapGestureListener.onTouchEvent(event);
+        }
+
         return !mGestureWaitForTouchSlop || mTracking;
     }
 

AOSP-whatever/platform_frameworks_base

(cherry picked from commit d2dffac684c6d31e5d66f1d12d5b5a815cd9776e)
Signed-off-by: Alex Naidis <alex.naidis@linux.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -23,10 +23,12 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.PowerManager;
 import android.os.SystemClock;
 import android.os.VibrationEffect;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.InputDevice;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
@@ -147,6 +149,8 @@ public abstract class PanelView extends FrameLayout {
     protected final SysuiStatusBarStateController mStatusBarStateController =
             (SysuiStatusBarStateController) Dependency.get(StatusBarStateController.class);
 
+    private GestureDetector mDoubleTapGestureListener;
+
     protected void onExpandingFinished() {
         mBar.onExpandingFinished();
     }
@@ -212,6 +216,18 @@ public abstract class PanelView extends FrameLayout {
         mFlingAnimationUtilsDismissing = new FlingAnimationUtils(context,
                 0.5f /* maxLengthSeconds */, 0.2f /* speedUpFactor */, 0.6f /* x2 */,
                 0.84f /* y2 */);
+
+        mDoubleTapGestureListener = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent event) {
+                final PowerManager pm = (PowerManager) context.getSystemService(
+                        Context.POWER_SERVICE);
+                pm.goToSleep(event.getEventTime());
+                return true;
+            }
+        });
+
         mBounceInterpolator = new BounceInterpolator();
         mFalsingManager = Dependency.get(FalsingManager.class);  // TODO: inject into a controller.
         mNotificationsDragEnabled =
@@ -402,6 +418,11 @@ public abstract class PanelView extends FrameLayout {
                 endMotionEvent(event, x, y, false /* forceCancel */);
                 break;
         }
+
+        if (mStatusBarStateController.getState() == StatusBarState.KEYGUARD) {
+            mDoubleTapGestureListener.onTouchEvent(event);
+        }
+
         return !mGestureWaitForTouchSlop || mTracking;
     }
 